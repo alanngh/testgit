d478a2f7a98cc3d8eff7dfaca6008d20c5c64eef
alanngh <alanngh@vt.edu>
2020-01-31 17:22:07 -0500

    modifyed on my mac

--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -4,5 +4,7 @@ int main(){
   printf("----------------\nHello world!!\n----------------\n");
   printf(" From OnDemand Cascate vt cluster\n");
   printf("--------------------------------------\n");
+  printf("Now on my laptop terminal");
+  printf("--------------------------------------\n");
   return 0;
 }
